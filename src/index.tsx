81607ce8c335a797017333e7eb27a5581cf15b3c
Ryo-1157 <hit.zjackpot.777@gmail.com>
2022-05-17 13:21:19 +0900

    useStateの使い方を復習

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,24 @@
 import * as React from "react";
 import { render } from "react-dom";
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import Editor from "./pages/editor";
 
-const Main = <h1>Markdown Editor</h1>;
+const GlobalStyle = createGlobalStyle`
+body * {
+  box-sizing: border-box;
+}
+`;
+
+const Header = styled.h1`
+  color: red;
+`;
+
+const Main = (
+  <>
+    <GlobalStyle />
+    <Editor />
+  </>
+);
 
 render(Main, document.getElementById("app"));
